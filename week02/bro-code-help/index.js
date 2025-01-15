380931a91247e7aa9e133650b91f6d813e51c955
tplumb <taylorwep@gmail.com>
2025-01-14 22:03:10 -0700

    codr bro help

---
+++ week02/bro-code-help/index.js
@@ -0,0 +1,14 @@
+console.log('Hello, Bro Code!');
+console.log('I like da burga!');
+
+/*
+window.alert('This is an alert!');
+window.alert('I like borger!');
+
+// This is the way to comment in JavaScript
+
+/* Anything after this is 
+a multi-line comment */
+
+document.getElementById('myH1').textContent = 'Heyo h1'
+document.getElementById('myP').textContent = 'I like more than borg'
