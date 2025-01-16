a9b9d3a321e1b935237add3ce1a729a5eaf06f77
tplumb <taylorwep@gmail.com>
2025-01-15 20:30:54 -0700

    favchap bom fixes

--- week02/bom-scripts/bomstyle.js
+++ week02/bom-scripts/bomstyle.js
@@ -1,9 +1,26 @@
-const input = document.querySelector('#favChap');
+const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('___________');
-const li = document.createElement('li');
-const deleteButton = document.createElement ('button');
-li.textContent = input.value;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-list.append(li);
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+        
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        
+        li.append(deleteButton);
+        list.append(li);
+        
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(li);
+            input.focus();
+        });
+        input.value = '';
+        input.focus();
+    }
+    else {
+        console.log('Please enter a chapter');
+        }
+});
